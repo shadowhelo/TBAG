16b78891a51e63b4b0a612c19f6d974551e95395
rhys909 <connorR@cardiff.ac.uk>
2018-10-23 13:27:13 +0100

    Books Added

--- items.py
+++ items.py
@@ -30,3 +30,59 @@ item_arkenstone = {
 
     "description": "A wondrous large white gem, the royal jewel of the Dwarf-kingdom of Erebor."
 }
+item_book1 = {
+    "id": "book 1",
+
+    "name": "pride and prejudice",
+
+    "description": "An old leather bound book."
+
+}
+item_book2 = {
+    "id": "book 2",
+
+    "name": "romeo and juliet",
+
+    "description": "An old leather bound book."
+
+}
+item_book3 = {
+    "id": "book 3",
+
+    "name": "the art of war",
+
+    "description": "An old leather bound book filled with great knowledge."
+
+}
+item_book4 = {
+    "id": "book 4",
+
+    "name": "great expectations",
+
+    "description": "An old leather bound book."
+
+}
+item_book5 = {
+    "id": "book 5",
+
+    "name": "a midsummer nights dream",
+
+    "description": "An old leather bound book."
+
+}
+item_book6 = {
+    "id": "book 6",
+
+    "name": "the great gatsby",
+
+    "description": "An old leather bound book."
+
+}
+item_book7 = {
+    "id": "book 7",
+
+    "name": "breakfast at tiffanys",
+
+    "description": "An old leather bound book."
+
+}
