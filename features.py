d43efa17f1e577abb97708f54b5834067c6f9a3f
shadowhelo <shadowhelo@hotmail.co.uk>
2018-10-24 09:02:56 +0100

    added features locations

--- features.py
+++ features.py
@@ -7,33 +7,41 @@ appropriately.
 """
 
 feature_bookcase = { 
-	"id": "bookcase",
+    "id": "bookcase",
 	
-	"name": "The bookcase",
+    "name": "The bookcase",
 
-	"description":  "An old oaken bookcase, it was once very fine indeed, but now it is wearing away."
+    "description":  "An old oaken bookcase, it was once very fine indeed, but now it is wearing away."
 }
 
 feature_dragon = {
-        "id": "dragon",
+    "id": "dragon",
 
-        "name": "The dragon",
+    "name": "The dragon",
 
-        "description": "A giant fucking dragon."
+    "description": "A giant fucking dragon."
 }
 
 feature_donkey = {
-        "id": "donkey",
+    "id": "donkey",
 
-        "name": "The donkey",
+    "name": "The donkey",
 
-        "description": "Looks like a bit of an ass"
+    "description": "Looks like a bit of an ass"
 }
 
 feature_wall_text = {
-	"id": "wall_text",
+    "id": "wall_text",
 
-	"name": "text on wall",
+    "name": "text on wall",
 
-	"description": "\nInscribed on the wall is a block of text saying, \n" + """"I HINT AT WHICH BOOK TO PICKUP." """
+    "description": "\nInscribed on the wall is a block of text saying, \n" + """"I HINT AT WHICH BOOK TO PICKUP." """
+}
+
+feature_rusty_gate = {
+    "id": "gate",
+
+    "name": "Rusty gate",
+
+    "description": "Is a locked and rusty gate"
 }
